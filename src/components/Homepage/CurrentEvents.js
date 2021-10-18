fc88f15ed29894e919d934af864d3671c493a4fe
Alejandro Arias <alejandroarias@Alejandros-MacBook-Air.local>
2021-10-17 19:30:42 -0500

    Added starter skeleton to display current events

--- src/components/Homepage/CurrentEvents.js
+++ src/components/Homepage/CurrentEvents.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Homepage.scss";
 import Fade from "react-reveal/Fade";
-import { requirePropFactory } from "@material-ui/core";
-import EventCard from "../Opportunities/EventCard";
-import { date } from "joi";
 
 function CurrentEvents(props) {
 
@@ -26,7 +23,10 @@ function CurrentEvents(props) {
                     <h3 className="seciton-title">Current Events</h3>   
                 </Fade>
                 <div className ="event-slider">
-                    
+                    /*
+                        Here I want to feed in the next 7 days events into the EventCard, and somehow
+                        add these instances of EventCard into a slider.
+                    */
                 </div>
             </div>
         </section>
