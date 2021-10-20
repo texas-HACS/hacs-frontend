a29e960ff55425bddc6b0ead28c5f3bf42da2862
Alejandro Arias <alejandroarias@Alejandros-Air.lan>
2021-10-20 18:01:15 -0500

    consolidating logic for displaying current events

--- src/components/Homepage/CurrentEvents.js
+++ src/components/Homepage/CurrentEvents.js
@@ -3,8 +3,22 @@ import "./Homepage.scss";
 import Fade from "react-reveal/Fade";
 import { requirePropFactory } from "@material-ui/core";
 import EventCard from "../Opportunities/EventCard";
+import { date } from "joi";
 
 function CurrentEvents(props) {
+
+    const today = new Date();
+    const dates = [today, new Date(today.getDate + 3), new Date(today.getDate - 3), new Date(today.getDate + 7), new Date(today.getDate + 8)];
+    const limit = new Date(today.getDate + 7);
+    // Filter the dates to get the valid date
+    // A date is valid iff it is between the current date, and the current date + 7 inclusive
+
+    const validDates = dates.filter((date) => {
+        if(date.getDate <= limit.getDate && today.getDate <= date.getDate) {
+            return date;
+        }
+    })
+    console.log(validDates);
     return(
         <section className="current-events">
             <div className="content-wrapper">
@@ -12,10 +26,7 @@ function CurrentEvents(props) {
                     <h3 className="seciton-title">Current Events</h3>   
                 </Fade>
                 <div className ="event-slider">
-                    /*
-                        Here I want to feed in the next 7 days events into the EventCard, and somehow
-                        add these instances of EventCard into a slider.
-                    */
+                    
                 </div>
             </div>
         </section>
