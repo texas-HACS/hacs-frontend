ac4a1d34785597e98a9e85f60adeea04e047a162
Hispanic-Association-of-CS <48233798+Hispanic-Association-of-CS@users.noreply.github.com>
2022-07-09 21:02:05 -0500

    Update source to google calender

--- src/components/Homepage/UpcomingEvents.js
+++ src/components/Homepage/UpcomingEvents.js
@@ -11,8 +11,8 @@ function UpcomingEvents(props) {
       </Fade>
       <Fade>
         <div className="calendar-wrapper flex">
-          <iframe
-            src="https://hacs-server.herokuapp.com/calendar"
+          <iframe 
+            src="https://calendar.google.com/calendar/embed?height=800&wkst=1&bgcolor=%234285F4&ctz=America%2FChicago&src=dGV4YXNoYWNzQGdtYWlsLmNvbQ&src=YWxlbG5lZmpkamwzbDlqbGQzdWI1ODAyZzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ODdqZnRpOGtha2JkcHI3MzRxYXV0NGw5cWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=cDM4ODBrNjdnYjM4NzJ0dGEycnFnZXYxcmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23bc7800&color=%23E67C73&color=%23039BE5&color=%239f00cc&color=%233F51B5"
             style={{
               border: 0,
               height: "600px",
@@ -24,7 +24,7 @@ function UpcomingEvents(props) {
             title="HACS Calendar"
           ></iframe>
           <iframe
-            src="https://hacs-server.herokuapp.com/calendar?agenda=true"
+            src="https://calendar.google.com/calendar/embed?height=800&wkst=1&bgcolor=%234285F4&ctz=America%2FChicago&src=dGV4YXNoYWNzQGdtYWlsLmNvbQ&src=YWxlbG5lZmpkamwzbDlqbGQzdWI1ODAyZzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ODdqZnRpOGtha2JkcHI3MzRxYXV0NGw5cWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=cDM4ODBrNjdnYjM4NzJ0dGEycnFnZXYxcmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23bc7800&color=%23E67C73&color=%23039BE5&color=%239f00cc&color=%233F51B5"
             style={{
               border: 0,
               height: "400px",
