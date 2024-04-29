4d61ba26cee3317c84b55e77eb39f472c7262546
Alexander Valdez <avaldez1030131@gmail.com>
2024-04-29 02:18:51 -0500

    Added structure for the people edit section

--- src/components/AdminPage/AlumniEdit.js
+++ src/components/AdminPage/AlumniEdit.js
@@ -1,10 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import { newUid } from "../utils/utils";
 
 function AlumniEdit(props) {
+    const [editing, setEditing] = useState(false);
+    const [data, setData] = useState({
+        ...props.data,
+        uid: props.data?.uid ?? newUid("alum"),
+    })
+
+    const handleSave = () => {
+        props.handleUpdate(data);
+        setEditing(false);
+        if (props.addNew) {
+          setData({ uid: newUid("alum") });
+        }
+      };
+    
+      const changeData = (key, value) => {
+        let newData = { ...data };
+        newData[key] = value;
+        setData(newData);
+      };
+    
+      const handleChange = (e) => {
+        let { name, value, type } = e.target;
+        value = value === "" ? null : value;
+        changeData(name, value);
+      };
+    
+    // need to implement the handle delete in adminpanel.js
+    //   const handleDelete = () => {
+    //     props.handleDelete(data.uid);
+    //     setEditing(false);
+    //   };
+    
+    //   const confirmDelete = () => {
+    //     if (window.confirm("Are you sure you want to delete this alum?")) {
+    //       handleDelete();
+    //     } else {
+    //       return;
+    //     };
+    //   };
+
+    const editSection = (
+        <div className="admin-edit form-wrapper">
+            <label>Name</label>
+            <input
+                id="name-edit"
+                className="form-control-small"
+                name="name"
+                defaultValue={data?.name}
+                required
+                onChange={handleChange}
+            />
+            <label>LinkedIn</label>
+            <input
+                id="linkedIn-edit"
+                className="form-control-small"
+                name="linkedin"
+                defaultValue={data?.linkedin}
+                required
+                onChange={handleChange}
+            />
+            <label>Alumni UID</label>
+            <input
+                id="alum-uid-edit"
+                className="form-control-small"
+                name="uid"
+                type="text"
+                value={data?.uid}
+                required
+                readOnly
+            />
+            <div className="button-container flex-row">
+            <button className="btn btn-primary" onClick={handleSave}>
+                {props.addNew ? "Create" : "Save"}
+            </button>
+            <button className="btn btn-primary" onClick={() => setEditing(false)} type="button">
+                {props.addNew ? "Cancel" : "Delete"}
+            </button>
+            </div>
+        </div>
+    )
+
+    const saveSection = (
+        <div onClick={() => setEditing(editing ^ true)}>
+          <p className="editable">
+            {data?.name == null ? (
+                "Add Alum"
+            ) : (
+              <span>
+                {data?.name}
+              </span>
+            )}
+          </p>
+        </div>
+      );
 
     return (
-        <div>
-            {props.data.name}
+        <div className="officer-edit editable-group">
+            {saveSection}
+            {!!editing && editSection}
         </div>
     )
 }
--- src/components/AdminPage/PeopleEdit.js
+++ src/components/AdminPage/PeopleEdit.js
@@ -8,8 +8,18 @@ function PeopleEdit(props) {
     let pastOfficerData;
     let alumniData;
 
+    const handleChange = () => {
+
+    }
+
+    const handleSave = () => {
+
+    }
+
     if (!props.addNew) {
         pastOfficerData = (
+            // might need to add check to prevent error if past officers is empty
+            // add a note that offices are added by clicking the archive button under the officer section
             Object.keys(props.data.pastOfficers)
                 .sort((a, b) => {
                     return props.data.pastOfficers[a].order - props.data.pastOfficers[b].order
@@ -26,6 +36,7 @@ function PeopleEdit(props) {
 
         alumniData = (
             <div>
+                {/* Might need to add a check to prevent errors if alumni data is empty */}
                 {Object.keys(props.data.alumni)
                     .sort((a, b) => {
                         return props.data.alumni[a].name.localeCompare(props.data.alumni[b].name)
@@ -47,15 +58,48 @@ function PeopleEdit(props) {
         )
     }
 
+    const editSection = (
+        <div className="admin-edit form-wrapper">
+            <label>Year 1</label>
+            <input
+                id="year1-edit"
+                className="form-control-small"
+                name="year1"
+                type="number"
+                required
+                onChange={handleChange}
+            />
+            <label>Year 2</label>
+            <input
+                id="year2-edit"
+                className="form-control-small"
+                name="year2"
+                type="number"
+                required
+                onChange={handleChange}
+            />
+            <div className="button-container flex-row">
+                <button className="btn btn-primary" onClick={handleSave}>
+                    Add
+                </button>
+                <button className="btn btn-primary" onClick={() => setOpen(false)} type="button">
+                    Cancel
+                </button>
+            </div>
+        </div>
+    )
+
     return (
         props.addNew 
             ? (
-                ""
+                <div>
+                    <h3 onClick={() => setOpen(!open)}>Add Year</h3>
+                    {!!open && editSection}
+                </div>
             ) 
             : (
                 <div>
                 <h3 onClick={() => setOpen(!open)}>{props.id}</h3>
-                {console.log(open)}
                 {!!open && pastOfficerData}
                 {!!open && alumniData}
                 </div>
